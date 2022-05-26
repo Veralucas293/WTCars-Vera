f9a9376ce397af449c9d128504526b9ea8ffe345
Lucas Vera <veralucas293@gmail.com>
2022-05-26 14:16:11 -0300

    master actualizado clase 2

---
+++ wtcars/src/components1/Nav.js
@@ -0,0 +1,15 @@
+const navbar= () =>{
+    return(
+        <div>
+           <button>Cuadros</button>
+           <button>Platos y palancas</button>
+           <button>Piñones</button>
+           <button>Cubiertas</button>
+           <button>Frenos y shifters</button>
+           <button>Stems</button>
+           <button>Horquillas</button>
+        </div>
+    )
+}
+
+export default navbar
---
+++ wtcars/src/container/ItemListContainer.js
@@ -0,0 +1,10 @@
+
+const ItemListContainer= (props) =>{
+    return(
+        <h1 style={{backgroundColor: 'grey'}}>{props.greeting}</h1>
+    )
+}
+
+
+
+export default ItemListContainer
